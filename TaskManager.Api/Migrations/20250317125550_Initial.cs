35f5fb7d2b1f4e6e8155ebc2a4ff53fad3184ab7
ImPanica <yarshevich89@gmail.com>
2025-03-20 15:22:40 +0300

    Create project

---
+++ TaskManager.Api/Migrations/20250317125550_Initial.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserStatus = table.Column<int>(type: "int", nullable: false),
+                    RegistrationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastLoginDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectAdmins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectAdmins", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectAdmins_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdminId = table.Column<int>(type: "int", nullable: true),
+                    ProjectStatus = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreateDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_ProjectAdmins_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "ProjectAdmins",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Desks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsPrivate = table.Column<bool>(type: "bit", nullable: false),
+                    Columns = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdminId = table.Column<int>(type: "int", nullable: true),
+                    ProjectId = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreateDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Desks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Desks_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Desks_Users_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectUser",
+                columns: table => new
+                {
+                    AllUsersId = table.Column<int>(type: "int", nullable: false),
+                    ProjectsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectUser", x => new { x.AllUsersId, x.ProjectsId });
+                    table.ForeignKey(
+                        name: "FK_ProjectUser_Projects_ProjectsId",
+                        column: x => x.ProjectsId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectUser_Users_AllUsersId",
+                        column: x => x.AllUsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    File = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    DeskId = table.Column<int>(type: "int", nullable: false),
+                    Column = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatorId = table.Column<int>(type: "int", nullable: true),
+                    ExecutorId = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreateDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Desks_DeskId",
+                        column: x => x.DeskId,
+                        principalTable: "Desks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Users_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Users_ExecutorId",
+                        column: x => x.ExecutorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Desks_AdminId",
+                table: "Desks",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Desks_ProjectId",
+                table: "Desks",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectAdmins_UserId",
+                table: "ProjectAdmins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_AdminId",
+                table: "Projects",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectUser_ProjectsId",
+                table: "ProjectUser",
+                column: "ProjectsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CreatorId",
+                table: "Tasks",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_DeskId",
+                table: "Tasks",
+                column: "DeskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_ExecutorId",
+                table: "Tasks",
+                column: "ExecutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectUser");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Desks");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "ProjectAdmins");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ TaskManager.Api/Program.cs
@@ -0,0 +1,124 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TaskManager.Api.Models;
+using TaskManager.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Добавляем контроллеры
+builder.Services.AddControllers();
+
+// Подключение к БД
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                          "Server=(localdb)\\mssqllocaldb;Database=TaskManagerDb;Trusted_Connection=True;";
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentityCore<User>().AddEntityFrameworkStores<ApplicationContext>();
+// Настройка JWT аутентификации
+// Добавляем сервис аутентификации в контейнер зависимостей.
+builder.Services.AddAuthentication(options =>
+    {
+        // Устанавливаем схему аутентификации по умолчанию для проверки подлинности токенов JWT.
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        // Устанавливаем схему аутентификации по умолчанию для вызова вызова (challenge) при неудачной аутентификации.
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+// Добавляем поддержку JWT-токенов в механизм аутентификации.
+    .AddJwtBearer(options =>
+    {
+        // Настройка параметров валидации JWT-токена.
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // Включаем проверку издателя токена (issuer). Это гарантирует, что токен был выпущен доверенным источником.
+            ValidateIssuer = true,
+
+            // Включаем проверку получателя токена (audience). Это гарантирует, что токен предназначен для нашего приложения.
+            ValidateAudience = true,
+
+            // Включаем проверку времени жизни токена. Это гарантирует, что токен не истёк.
+            ValidateLifetime = true,
+
+            // Включаем проверку ключа подписи токена. Это гарантирует, что токен был подписан доверенным ключом.
+            ValidateIssuerSigningKey = true,
+
+            // Указываем допустимого издателя токена (issuer), который берётся из конфигурации приложения.
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+
+            // Указываем допустимую аудиторию токена (audience), которая также берётся из конфигурации приложения.
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            // Указываем ключ, используемый для подписи токена. 
+            // Ключ берётся из конфигурации приложения и преобразуется в массив байтов с использованием UTF-8 кодировки.
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+// Добавляем PasswordHasher в контейнер зависимостей
+builder.Services.AddScoped<PasswordHasher<User>>();
+//builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationContext>();
+        var passwordHasher = services.GetRequiredService<PasswordHasher<User>>();
+
+        // Создаем базу данных, если она не существует
+        context.Database.EnsureCreated();
+
+        // Проверяем есть ли админ в системе
+        if (!context.Users.Any(u => u.UserStatus == UserStatus.Admin))
+        {
+            var admin = new User
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Login = "Admin",
+                Email = "admin@gmail.com",
+                Password = "123", // Временный пароль
+                Phone = "+79093070777",
+                UserStatus = UserStatus.Admin,
+                RegistrationDate = DateTime.Now
+            };
+
+            // Хешируем пароль
+            admin.Password = passwordHasher.HashPassword(admin, "123");
+
+            context.Users.Add(admin);
+            context.SaveChanges();
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ошибка при инициализации базы данных.");
+    }
+}
+
+// Настраиваем маршрутизацию
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+// Запуск приложения
+app.Run();
